3afe7c6108bcf43ece300e429da448844430aa43
Luísa Magalhães <lfm3@cesar.school>
2026-04-26 19:07:59 -0300

    comecei a funcao criar_alien

--- alien.c
+++ alien.c
@@ -1,3 +1,13 @@
 #include "alien.h"
 #include <stdlib.h>
 
+Alien *criar_alien(int tipo, int linha){
+    Alien *a= (Alien *) malloc(sizeof(Alien));
+    if (a == NULL){
+        return NULL;
+    }
+    a->tipo = tipo;
+    a->linha = linha;
+    a->coluna = 8;
+    a->next = NULL;
+}

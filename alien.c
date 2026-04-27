48cdc163492f60675ef863db89148907a9f2412d
Luísa Magalhães <lfm3@cesar.school>
2026-04-27 18:21:59 -0300

    adicionei a função criar alien

--- alien.c
+++ alien.c
@@ -10,4 +10,23 @@ Alien *criar_alien(int tipo, int linha){
     a->linha = linha;
     a->coluna = 8;
     a->next = NULL;
+
+    switch (tipo){
+        case ALIEN_INVASOR:
+            a->vida = 10;
+            a->velocidade = 1;
+            a->dano = 10;
+            break;
+        case ALIEN_BLINDADO:
+            a->vida = 100;
+            a->velocidade = 1;
+            a->dano = 20;
+            break;
+        case ALIEN_KAMIKAZE:
+            a->vida = 10;
+            a->velocidade = 2;
+            a->dano = 100;
+            break;
+        }
+    return a;
 }

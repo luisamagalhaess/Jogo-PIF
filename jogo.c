d429ec61f5ca6d28b0b7df46eec310b2576777fa
Luísa Magalhães <lfm3@cesar.school>
2026-04-28 17:58:49 -0300

    base do jogo

--- jogo.c
+++ jogo.c
@@ -7,4 +7,30 @@ void iniciar_jogo(Jogo* j){
             j->grid[l][c].defesa = NULL;
         }
     }
+
+    for(int l = 0; l < LINHAS; l++){
+        j->aliens[l] = NULL;
+    }
+
+    j->energia = 50;
+    j->vidas = 5;
+    j->score = 0;
+    j->onda_atual = 1;
+    j->cursor_linha = 0;
+    j->cursor_coluna = 0;
+}
+
+void destruir_jogo(Jogo* j){ //liberar a memória alocada
+    for(int l = 0; l < LINHAS; l++){
+        for(int c = 0; c < COLUNAS; c++){
+            if(j->grid[l][c].defesa != NULL){
+                free(j->grid[l][c].defesa);
+                j->grid[l][c].defesa = NULL;
+            }
+        }
+    }
+
+    for (int l = 0; l < LINHAS; l++) {
+        destruir_lista(&j->aliens[l]);
+    }
 }

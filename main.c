c473e36774ae956e593a6ba688f2228b80748398
Laura Vitória <lvsa@cesar.school>
2026-05-08 19:11:12 -0300

    feat: migração para raylib, renderização do grid e remoção da cli-lib

--- main.c
+++ main.c
@@ -2,9 +2,7 @@
 #include <string.h>
 #include "jogo.h"
 #include "scores.h"
-#include "screen.h"
-#include "keyboard.h"
-#include "timer.h"
+#include "raylib.h"
 
 int main() {
     // 1. Declarar variáveis
@@ -14,99 +12,37 @@ int main() {
     char nome[50];
 
     // 2. Inicializar
-    screenInit(1);
-    keyboardInit();
+    InitWindow(800, 600, "Erid");
     iniciar_jogo(&j);
     carregar_scores(scores, &scores_salvos);
 
-    // 3. Pedir nome do jogador
-    keyboardDestroy(); // desativa modo especial do teclado temporariamente
-    screenGotoxy(10, 5);
-    printf("Digite seu nome: ");
-    fgets(nome, 50, stdin);
-    nome[strcspn(nome, "\n")] = '\0'; // remove o \n do final
-    keyboardInit(); // reativa
+    int x_inicial = (800 - 9 * 64) / 2;
+    int y_inicial = 100;
 
-    // 4. Loop do jogo
-    while (j.vidas > 0) {
-        if (keyhit()) {
-            int tecla = readch();
-        
-            if (tecla == 'w') {
-                if (j.cursor_linha > 0){ //só move se não estiver na primeira linha
-                    j.cursor_linha--;
+    
+    //loop
+    while (!WindowShouldClose()) {
+        Vector2 mouse = GetMousePosition();
+        int col_mouse = (mouse.x - x_inicial) / 64;
+        int lin_mouse = (mouse.y - y_inicial) / 64;
+        BeginDrawing();
+            ClearBackground(BLACK);
+            for (int l = 0; l < LINHAS; l++) {
+                for (int c = 0; c < COLUNAS; c++) {
+                    Color cor = DARKGRAY;
+                    if (l == lin_mouse && c == col_mouse) {
+                            cor = GRAY;
+
+}
+
+                    DrawRectangle(x_inicial + c * 64, y_inicial + l * 64, 64, 64, cor);
+                    DrawRectangleLines(x_inicial + c * 64, y_inicial + l * 64, 64, 64, WHITE);
                 }
             }
-
-            if (tecla == 's') {
-                if (j.cursor_linha < LINHAS - 1){ // se não chegou na última linha
-                    j.cursor_linha++;
-                }
-            }
-
-            if (tecla == 'a'){
-                if (j.cursor_coluna > 0){ //só move se não estiver na primeira coluna
-                    j.cursor_coluna--;
-                }
-            }
-
-            if (tecla == 'd'){
-                if (j.cursor_coluna < COLUNAS - 1){ // se não chegou na última coluna
-                    j.cursor_coluna++;
-                }
-            }
-
-            if (tecla == '1'){
-                if (j.energia >= 10){
-                    Defesa *d = criar_defesa(DEFESA_GERADOR); // cria e guarda
-                    j.grid[j.cursor_linha][j.cursor_coluna].defesa = d; // coloca no grid
-                    j.energia = j.energia - 10; // desconta energia
-                }
-            }
-
-            if (tecla == '2'){
-                if (j.energia >= 100){
-                    Defesa *d = criar_defesa(DEFESA_TORRETA); // cria e guarda
-                    j.grid[j.cursor_linha][j.cursor_coluna].defesa = d; // coloca no grid
-                    j.energia = j.energia - 100; // desconta energia
-                }
-            }
-
-            if (tecla == '3'){
-                if (j.energia >= 10){
-                    Defesa *d = criar_defesa(DEFESA_MURO); // cria e guarda
-                    j.grid[j.cursor_linha][j.cursor_coluna].defesa = d; // coloca no grid
-                    j.energia = j.energia - 10; // desconta energia
-                }
-            }
-
-            if (tecla == '4'){
-                if (j.energia >= 100){
-                    Defesa *d = criar_defesa(DEFESA_BOMBA); // cria e guarda
-                    j.grid[j.cursor_linha][j.cursor_coluna].defesa = d; // coloca no grid
-                    j.energia = j.energia - 100; // desconta energia
-                }
-            }
-
-            if (tecla == 'r'){
-                if (j.grid[j.cursor_linha][j.cursor_coluna].defesa != NULL){ //checa se tem alguma defesa na célula
-                    destruir_defesa(j.grid[j.cursor_linha][j.cursor_coluna].defesa); //libera a memória
-                    j.grid[j.cursor_linha][j.cursor_coluna].defesa = NULL; //celula fica vazia
-                } 
-            }
-
-            if (tecla == 'q') {
-                break;
-            }
-        }
-
-
-
-
-// o jogo continua independente de ter tecla ou não
+        EndDrawing();
     }
     
-
+    
     // 5. Game over
     Score novo_score;
     strcpy(novo_score.nome, nome);
@@ -115,8 +51,7 @@ int main() {
     exibir_scores(scores, scores_salvos);
 
     // 6. Liberar memória
-    keyboardDestroy();
-    screenDestroy();
+    CloseWindow();
     destruir_jogo(&j);
     return 0;
 }
